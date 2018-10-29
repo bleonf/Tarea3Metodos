2a1ffe3c63faaba53e719cec6c0c0cc0bcc8dc00
bleonf <42364547+bleonf@users.noreply.github.com>
2018-10-28 22:31:43 -0500

    Add files via upload

---
+++ Fourier.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pylab as plt
+from numpy import linalg 
+
+def fourier():
+	
---
+++ Fourier2D.py
@@ -0,0 +1,5 @@
+import numpy as np
+import matplotlib.pylab as plt
+from numpy import linalg 
+
+def fourier():

ecb1dd105031486d79f9272203f99e7d14ee732d
bleonf <42364547+bleonf@users.noreply.github.com>
2018-10-30 19:12:02 -0500

    Add files via upload

--- Fourier2D.py
+++ Fourier2D.py
@@ -7,31 +7,36 @@ img = plt.imread("arbol.png")
 #retorna matriz de numpy de 256x256
 
 
-realfourier=(fft.ifft2(img)).real
-#parte real
-imagfourier=(fft.ifft2(img)).imag
-#parte imaginara
+
 fouriertotal=(fft.ifft2(img))
 
-frecuencia=np.fft.fftfreq(len(fouriertotal[0]))
+frecuencia=np.fft.fftfreq(len(fouriertotal[0].real))
 
 plt.figure("Fourier")
 plt.plot(frecuencia,fouriertotal)
 plt.savefig("LeonBenjamin_FT2D.pdf")
 plt.show()
+print fouriertotal
 
-
-
+#i=0
+#j=0
+#while j<256:
+#	while i<256:
+#		if ((fouriertotal[i,j]>0.03) and (fouriertotal[i,j]<0.08)):
+#			fouriertotal[i,j]=0
+#			i=i+1
+#		else:
+#			i=i+1
+#
+#	j=j+1	
 i=0
-j=0
-while j<256:
-	while i<256:
-		if ((fouriertotal[i,j]>0.03) and (fouriertotal[i,j]<0.08)):
-			fouriertotal[i,j]=0
+while i<256:
+		if ((abs(frecuencia[i].real)>0.24 and abs(frecuencia[i].real)<0.26)or(abs(frecuencia[i].real)>0.038 and abs(frecuencia[i].real)<0.043)):
+#		if ((fouriertotal[i].real.any()>0.03) and (fouriertotal[i].real.any()<0.08)):
+			fouriertotal[i]=0
 			i=i+1
 		else:
 			i=i+1
-	j=j+1	
 plt.figure("Fourier Filtrada")
 plt.plot(frecuencia,fouriertotal)
 plt.savefig("LeonBenjamin_FT2D_filtrada.pdf")
